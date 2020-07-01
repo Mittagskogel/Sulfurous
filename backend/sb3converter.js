1b8832f922ef5c46fa4ab03e8f84cc6b4bdee257
Alexander Pichler <alexnader.pichler.fi@gmail.com>
2020-07-01 13:39:37 +0200

    fixed problem with existing converted files

--- backend/sb3converter.js
+++ backend/sb3converter.js
@@ -11,10 +11,10 @@ var getProjectJSON = async function (projectID) {
     return req.json()
 }
 
-var getAssets = async function(sb3Name){
+var getAssets = async function (sb3Name) {
 
     //console.log(assetsBaseURL + sb3Name+"/get/")
-    const req = await fetch(assetsBaseURL + sb3Name+"/get/");
+    const req = await fetch(assetsBaseURL + sb3Name + "/get/");
     return req.buffer()
 
 }
@@ -34,8 +34,8 @@ var convertFromFile = function (file) {
         if (fileName == "project.json") {
             var fileContent = zip.readAsText(fileName)
 
-           // out = parser.convert(JSON.parse(fileContent), filemap);
-           out = undefined;
+            // out = parser.convert(JSON.parse(fileContent), filemap);
+            out = undefined;
             return;
         }
     });
@@ -62,61 +62,61 @@ var convertFromID = function (projectID) {
     return new Promise((resolve, reject) => {
         console.log("[CONVERTER] started converting " + projectID + " from ID")
 
-        var files = fs.readdirSync('./sb2/');
-       // console.log(files)
-        files.forEach(element => {
-            if (element == projectID + ".sb2") {
-              //  console.log(projectID + ".sb2")
-                fs.readFile('./sb2/'+projectID + ".sb2", function (err, data) {
-                    if (err) throw err;
-                    console.log("[CONVERTER] found converted project " + projectID)
-                    resolve(data.buffer);
-                  });
+
+
+
+        fs.readFile('./sb2/' + projectID + ".sb2", function (err, data) {
+            if (err) {
+                // throw err;
+                var newZip = new AdmZip();
+
+                getProjectJSON(projectID).then(res => {
+
+                    var filemap = parseMap(res.targets);
+                    //console.log(JSON.stringify(res,null,2));
+
+                    newZip.addFile("project.json", JSON.stringify(res));
+
+                    Object.keys(filemap).forEach(sb3Name => {
+                        // var sb2Name = filemap[element];
+
+
+                        getAssets(sb3Name).then(asset => {
+                            newZip.addFile(sb3Name, Buffer.alloc(Buffer.from(asset).length, asset));
+                        }).then(function () {
+                            if (Object.keys(filemap).length == newZip.getEntries().length - 1) {
+                                newZip.writeZip("./sb3/" + projectID + ".sb3");
+                                let findsb2 = setInterval(() => {
+                                    var files = fs.readdirSync('./sb2/');
+                                    // console.log(files)
+                                    files.forEach(element => {
+                                        if (element == projectID + ".sb2") {
+                                            // console.log(projectID + ".sb2")
+                                            fs.readFile('./sb2/' + projectID + ".sb2", function (err, data) {
+                                                if (err) throw err;
+                                                console.log(data.buffer);
+                                                resolve(data.buffer);
+                                            });
+                                            clearInterval(findsb2);
+                                        }
+                                    });
+                                }, 1000);
+
+
+                                console.log("[CONVERTER] finished converting " + projectID + " from ID")
+                            }
+                        })
+                    })
+                })
             }
+            console.log("[CONVERTER] found converted project " + projectID)
+            resolve(data.buffer);
         });
 
 
-        var newZip = new AdmZip();
-        
-        getProjectJSON(projectID).then(res => {
-
-            var filemap = parseMap(res.targets);
-            //console.log(JSON.stringify(res,null,2));
-
-            newZip.addFile("project.json", JSON.stringify(res));
-
-            Object.keys(filemap).forEach(sb3Name => {
-               // var sb2Name = filemap[element];
-                
-
-                getAssets(sb3Name).then(asset => {
-                    var temp = newZip.getEntries().length;
-                    newZip.addFile(sb3Name, Buffer.alloc(Buffer.from(asset).length, asset));
-                }).then(function () {
-                    if (Object.keys(filemap).length == newZip.getEntries().length - 1) {
-                        newZip.writeZip("./sb3/" + projectID + ".sb3");
-                        let findsb2 = setInterval(() => {
-                            var files = fs.readdirSync('./sb2/');
-                           // console.log(files)
-                            files.forEach(element => {
-                                if (element == projectID + ".sb2") {
-                                   // console.log(projectID + ".sb2")
-                                    fs.readFile('./sb2/'+projectID + ".sb2", function (err, data) {
-                                        if (err) throw err;
-                                        console.log(data.buffer);
-                                        resolve(data.buffer);
-                                      });
-                                    clearInterval(findsb2);
-                                }
-                            });
-                        }, 1000);
 
 
-                        console.log("[CONVERTER] finished converting " + projectID + " from ID")
-                    }
-                })
-            })
-        })
+
     });
 }
 
