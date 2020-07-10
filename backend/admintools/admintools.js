ceb4710fd1808228dbf504d890ef5fe1cba5721a
Alexander Pichler <alexnader.pichler.fi@gmail.com>
2020-07-10 10:16:00 +0200

    fixed counter of dashboard

--- backend/admintools/admintools.js
+++ backend/admintools/admintools.js
@@ -24,7 +24,7 @@ var setupAdminTools = function (io) {
 
 
 
-
+    calculateConnectionNumbers()
     console.log(logs)
 
     console.log("ADMINTOOLS SCRIPT")
@@ -32,18 +32,21 @@ var setupAdminTools = function (io) {
 
 
 function calculateConnectionNumbers() {
+    console.log({ "lastHour": logs.requests.lastHour, "last24Hours": logs.requests.last24Hours, "last30Days": logs.requests.last30Days, "currentConnections": logs.currentConnections })
     logs.requests.lastHour = 0;
     logs.requests.last24Hours = 0;
     logs.requests.last30Days = 0;
     logs.requests.all.forEach(element => {
-        // console.log(element)
-        if (element.timestamp > (new Date() - 3600 * 10000)) {
+        //  console.log(element)
+        //console.log(new Date(element.timestamp).getTime() + "  " + (new Date().getTime() + (3600 * 1000)) + "  " + (new Date() - new Date(element.timestamp).getTime()))
+
+        if ((new Date() - new Date(element.timestamp).getTime()) < (3600 * 1000)) {
             logs.requests.lastHour++;
         }
-        if (element.timestamp > (new Date() - 3600 * 10000 * 24)) {
+        if ((new Date() - new Date(element.timestamp).getTime()) < (3600 * 1000 * 24)) {
             logs.requests.last24Hours++;
         }
-        if (element.timestamp > (new Date() - 3600 * 10000 * 24 * 30)) {
+        if ((new Date() - new Date(element.timestamp).getTime()) < (3600 * 1000 * 24 * 30)) {
             logs.requests.last30Days++;
         }
     });
